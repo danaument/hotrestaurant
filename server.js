c87257eefc491b0d9b243c2b64260db129c72715
Dan Aument <danaument@gmail.com>
2020-12-21 19:57:43 -0600

    moved example to waitlist

--- server.js
+++ server.js
@@ -9,18 +9,17 @@ app.use(express.json());
 
 var currentTables = [
     {
-        name: '',
-        partySize: 0,
-        contact: '',
+        
     }
 ]
 
 var waitList = [
     {
-
+        name: 'example',
+        partySize: 0,
+        contact: 'testcontact',
     }
 ]
-
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
@@ -33,7 +32,25 @@ app.get("/make", function(req, res) {
 app.get("/view", function(req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
 });
+
+app.get("/api/currentTables", function(req, res) {
+    return res.json(currentTables);
+});
+
+app.get("/api/waitList", function(req, res) {
+    return res.json(waitList);
+});
   
 app.post("/submit", function(req, res) {
+    var newReservation = req.body;
 
+    if (currentTables.length < 5) {
+        currentTables.push(newreservation);
+    } else {
+        waitList.push(newReservation);
+    }
 })
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
